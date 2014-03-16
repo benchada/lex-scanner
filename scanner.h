2b7d8a2dea8b3ec91c1d3b498abee6404043427b
benchada <manel.benmahcene@colorado.edu>
2014-03-16 12:22:23 -0700

    value of tokens are generated correctly

---
+++ scanner.h
@@ -0,0 +1,5 @@
+
+enum OpType { ADD=1, SUB, MUL, DIV, EQ, NE, LT, LE, GT, GE, ASGN }; /*Operator Type*/
+enum TokenType { IF_TK=300, THEN_TK, ELSE_TK, WHILE_TK, DO_TK, END_TK, READ_TK, WRITE_TK, ID, ADDOP, MULOP, RELOP, ASGNOP, INTLIT, STRLIT,
+		BRACKET_TK, COMM, SEMI_COLUMN  }; /* Tokens Type*/
+
